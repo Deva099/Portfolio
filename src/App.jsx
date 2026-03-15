78e09b59bb1f74a2e3b594d97684a56233f1a31d
Dibakar Pradhan <devapradhan130@gmail.com>
2026-03-16 01:02:24 +0530

    Fix: Restore accidentally removed imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import React from 'react'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Background from './components/layout/Background'
+import SmoothScroll from './components/layout/SmoothScroll'
+import ScrollToTop from './components/layout/ScrollToTop'
+import Home from './pages/Home'
+import ProjectsPage from './pages/ProjectsPage'
+import ContactPage from './pages/ContactPage'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Terms from './pages/Terms'
+
 /**
  * App.jsx
  * The main entry point of the application. 
